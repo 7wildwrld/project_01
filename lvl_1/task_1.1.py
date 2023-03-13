bbf8f210057ba0fd2ee9c453089368ec9be075f1
7wildwrld <qlebyaka7@yandex.ru>
2023-03-13 16:09:44 +0300

    remove directory

--- lvl_1/task_1.1.py
+++
@@ -1,6 +0,0 @@
-my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
-string_length = len(my_favorite_songs)
-print(my_favorite_songs[0:14], end=' ')
-print(my_favorite_songs[-13:], end=' ')
-print(my_favorite_songs[16:30], end=' ')
-print(my_favorite_songs[-26:-15], end=' ')
