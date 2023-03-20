2b5853ddcad45e77d4d7d2376da862cf4d8f6403
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-03-20 12:09:30 +0300

    Выполнено

--- lvl_2/task_2.1.py
+++ lvl_2/task_2.1.py
@@ -19,3 +19,31 @@ for i in input('Введите список чисел: ').split(','):
     listNum.append(int(i))
 minimum(listNum)
 maximum(listNum)
+
+# Хорошо! У вас вариант с сортировкой выбором
+# Решение задачи с помощью быстрой сортировки 
+
+def quicksort(arr):
+    if len(arr) < 2:
+        # базовый случай, массив с 0 или 1 элементом уже отсортирован
+        return arr
+    else:
+        # рекурсивный случай. выберем опорный элемент pivot
+        pivot = arr[0]
+        # подмассив всех элементов, меньших, чем опорный элемент pivot
+        less = [i for i in arr[1:] if i <= pivot]
+        # подмассив всех элементов, превышающих опорный элемент pivot
+        greater = [i for i in arr[1:] if i > pivot]
+        # обединяем в порядке "сортировка для меньшего подмассива – опорный элемент – сортировка для большего подмассив"
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+# Максимум - это последний элемент отсортированного списка
+def maximum(arr):
+    return quicksort(arr)[-1]
+
+ # Минимум - это первый элемент отсортированного списка
+def minimum(arr):
+    return quicksort(arr)[0]
+
+print('min =', minimum([4,6,2,1,9,63,-134,566]))
+print('max =', maximum([4,6,2,1,9,63,-134,566]))
